54b46c347432f45544b9a120b433515022066ba9
sala03 <sala03@SL03PC09.targettrust.local>
2017-03-03 23:33:22 -0300

    Aula

---
+++ Aula-02-04/src/main/java/br/com/tt/Programa.java
@@ -0,0 +1,11 @@
+package br.com.tt;
+
+import static java.lang.Math.PI;
+import static java.lang.System.*;
+
+public class Programa {
+	public static void main(String[] args) {
+		out.println("teste");
+		double pi = PI;
+	}
+}
