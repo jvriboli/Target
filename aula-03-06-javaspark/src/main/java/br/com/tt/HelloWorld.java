3b79d2f4b490ec3af65c31ce67b45d5790afcd81
sala03 <sala03@SL03PC09.targettrust.local>
2017-03-06 23:32:06 -0300

    Aula

---
+++ aula-03-06-javaspark/src/main/java/br/com/tt/HelloWorld.java
@@ -0,0 +1,15 @@
+package br.com.tt;
+
+import static spark.Spark.get;
+
+import java.util.Date;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		String page1 = "<h1><center>Minha Pag";
+		page1 += "</h1></center>";
+		page1 += "Data: " + new Date();
+		final String htmlPage = page1;
+		get("/hello", (req, res) -> htmlPage);
+	}
+}
